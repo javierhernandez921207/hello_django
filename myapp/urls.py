cffcca1af61a3b0f1777c0bbb97ea3287acff414
Javier Hernandez <javier.hernandez@ntsprint.com>
2024-02-21 21:45:32 -0500

    add crud project and task

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index ),
-    path('about', views.about ),
-    path('hello/<str:user>', views.hello),
-    path('projects', views.projects),
-    path('tasks', views.tasks),
-    path('task/<int:id>', views.task),
-    path('task/<str:name>', views.taskName),
-    path('task_create', views.create_task)
+    path('', views.index , name='index'),
+    path('about', views.about, name='about'),
+    path('hello/<str:user>', views.hello, name='hello'),
+    path('projects', views.projects, name='projects'),
+    path('project_create', views.create_project, name='project_create'),
+    path('tasks', views.tasks, name='tasks'),
+    path('task/<int:id>', views.task, name='task'),
+    path('task_create', views.create_task, name='task_create')
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Project, Task
-from .forms import CreateNewTask
+from .forms import CreateNewProject, CreateNewTask
 # Create your views here.
 def index(request):
     title = 'My App'
@@ -15,14 +15,51 @@ def hello(request, user):
 
 def projects(request):
     projects  = list(Project.objects.values())
-    return render(request, 'projects.html', {'projects': projects})
+    return render(request, 'projects/projects.html', {'projects': projects})
 
 def tasks(request):
     tasks = list(Task.objects.values())
-    return render(request, 'tasks.html', {'tasks': tasks})
+    return render(request, 'tasks/tasks.html', {'tasks': tasks})
 
 def create_task(request):
-    return render(request, 'task_create.html', {'form': CreateNewTask()})
+    if request.method == 'POST':
+        form = CreateNewTask(request.POST)
+        if form.is_valid():
+            task = Task(title=form.cleaned_data['title'], description=form.cleaned_data['description'], project_id=1)
+            task.save()
+            return redirect('tasks')
+        else:
+            return render(request, 'tasks/task_create.html', {'form': CreateNewTask()})
+    return render(request, 'tasks/task_create.html', {'form': CreateNewTask()})
+
+def create_project(request):
+    if request.method == 'POST':
+        form = CreateNewProject(request.POST)
+        if form.is_valid():
+            project = Project(name=form.cleaned_data['title'])
+            project.save()
+            return redirect('projects')
+        else:
+            return render(request, 'projects/project_create.html', {'form': CreateNewProject()})
+    return render(request, 'projects/project_create.html', {'form': CreateNewProject()})
+
+def update_task(request, id):
+    task = get_object_or_404(Task,id=id)
+    if request.method == 'POST':
+        form = CreateNewTask(request.POST)
+        if form.is_valid():
+            task.title = form.cleaned_data['title']
+            task.description = form.cleaned_data['description']
+            task.save()
+            return redirect('tasks')
+        else:
+            return render(request, 'tasks/task_update.html', {'form': CreateNewTask(), 'task': task})
+    return render(request, 'tasks/task_update.html', {'form': CreateNewTask(), 'task': task})
+
+def delete_task(request, id):
+    task = get_object_or_404(Task,id=id)
+    task.delete()
+    return redirect('tasks')
 
 def task(request, id):
     task = get_object_or_404(Task,id=id)    
